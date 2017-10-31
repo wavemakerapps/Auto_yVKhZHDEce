27593d8592a81d44a4dd2e21822929d71c3167a6
apptest <appTest1@wavemaker.com>
2017-10-31 21:53:25 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_yVKhZHDEce",
+  "homePage" : "Main",
+  "name" : "Auto_yVKhZHDEce",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
